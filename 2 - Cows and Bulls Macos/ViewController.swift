4bff480aa4e201ef9fb6be51e8bb26c5f8ca4e47
Baris <karalarbaris@gmail.com>
2021-12-07 07:54:15 +0100

    Initial Commit

---
+++ 2 - Cows and Bulls Macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  2 - Cows and Bulls Macos
+//
+//  Created by Baris Karalar on 7.12.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
