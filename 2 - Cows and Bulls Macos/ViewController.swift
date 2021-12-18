3be6d0449086d431fd7b8046b8c57fafadf4d296
Baris <karalarbaris@gmail.com>
2021-12-18 15:27:14 +0300

    Marking guesses

--- 2 - Cows and Bulls Macos/ViewController.swift
+++ 2 - Cows and Bulls Macos/ViewController.swift
@@ -8,7 +8,7 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     @IBOutlet var tableView: NSTableView!
     @IBOutlet var guess: NSTextField!
     
@@ -24,19 +24,56 @@ class ViewController: NSViewController {
         startNewGame()
         
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
     
     @IBAction func submitGuess(_ sender: NSButton) {
-    
+        
+        // check for 4 unique characters
+        let guessString = guess.stringValue
+        guard Set(guessString).count == 4 else { return }
+        guard guessString.count == 4 else { return }
+        
+        // ensure there are no non-digit characters
+           let badCharacters = CharacterSet(charactersIn:"0123456789").inverted
+           guard guessString.rangeOfCharacter(from: badCharacters) == nil else { return }
+        
+        // add the guess to the array and table view
+        guesses.insert(guessString, at: 0)
+        tableView.insertRows(at: IndexSet(integer: 0), withAnimation: .slideDown)
+        
+        // did the player win?
+        let resultString = result(for: guessString)
+        if resultString.contains("4b") {
+           let alert = NSAlert()
+           alert.messageText = "You win"
+           alert.informativeText = "Congratulations! Click OK to play again."
+           alert.runModal()
+           startNewGame()
+        }
     }
     
     func result(for guess: String) -> String {
-        return "result"
+        
+        var bulls = 0
+        var cows = 0
+        
+        let guessLetters = Array(guess)
+        let answerLetters = Array(answer)
+        
+        for (index, letter) in guessLetters.enumerated() {
+            if letter == answerLetters[index] {
+                bulls += 1
+            } else if answerLetters.contains(letter) {
+                cows += 1
+            }
+        }
+        
+        return "\(bulls)b \(cows)c"
     }
     
     func startNewGame() {
@@ -50,11 +87,11 @@ class ViewController: NSViewController {
         for _ in 0 ..< 4 {
             answer.append(String(numbers.removeLast()))
         }
-        
+//        print(answer)
         tableView.reloadData()
     }
     
-
+    
 }
 
 extension ViewController: NSTableViewDelegate {
